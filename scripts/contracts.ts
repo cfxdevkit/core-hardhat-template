74bf818ab85812853af46c8efc4b7ebfe4bd28c1
cfxdevkit <spcfxda+cdk@gmail.com>
2024-12-19 10:57:19 +0100

    Update contracts.ts

--- scripts/contracts.ts
+++ scripts/contracts.ts
@@ -2,6 +2,7 @@ import hre from "hardhat";
 
 async function main() {
   const myToken = await hre.cive.deployContract("MyToken", [1_000_000n]);
+  console.log("MyToken address:", myToken.address)
   const initialSupply = await myToken.read.totalSupply();
   console.log(`Initial supply of MyToken: ${initialSupply}`); // Initial supply of MyToken: 1000000
   const hash = await myToken.write.increaseSupply([500_000n]);
